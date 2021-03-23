bf1353f81de59e00a4206731e0362d2b980c5191
LovinduSaiGopinath <81213396+LovinduSaiGopinath@users.noreply.github.com>
2021-03-23 19:48:44 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,6 +4,7 @@ public static void main(String[] args)
     {
         //This is Thirupal file
         //Will this work
+    // this is changed by Gopinath
         System.out.println("hello, world");
     }
 }
